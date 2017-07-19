d2c8ced319fa096c4b1c4ae5f71144d7d63201f9
Matheus Fidelis <msfidelis@outlook.com>
2017-07-18 21:21:42 -0300

    routes

---
+++ client/routes.js
@@ -0,0 +1,14 @@
+Router.route('/', {
+    name: 'home',
+    template: 'home'
+});
+
+Router.route('/about', {
+    name: 'about',
+    template: 'about'
+});
+
+Router.route('/todo', {
+    name: 'todo',
+    template: 'todo'
+});
